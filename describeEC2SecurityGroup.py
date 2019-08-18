d4a4fdb4dc2823537a5eb5f6dc8c2f97abcbcb0f
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2019-08-18 12:16:44 +0530

    Update describeEC2SecurityGroup.py
    Updated code to use DRY approach to retrieve paginated calls

--- describeEC2SecurityGroup.py
+++ describeEC2SecurityGroup.py
@@ -3,55 +3,65 @@ import json
 
 client = boto3.client('ec2')
 
-# Enter Target VPC ID
-target_vpc_id = '<Enter-VPC-id>'
+target_vpc_id = '<Enter-VPC=ID>'
 
+# Create an array to store the instance id's to loop over later
+all_instances = []
+
+# Function defination outside the handler for faster function execution
+# This will get all the required metadata - instance id, associated subnet and the NACL id for the target VPC.
+def instanceMetadata(instance, result):
+    # Check if the instance is in the Target VPC:
+    if(result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['VpcId'] == target_vpc_id):
+        instance_id = result['Reservations'][instance]['Instances'][0]['InstanceId']
+        security_group_id = result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['Groups'][0]['GroupId']
+        subnet_id = result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['SubnetId']
+        
+        response = client.describe_network_acls(
+            Filters=[
+                {
+                    'Name': 'association.subnet-id',
+                    'Values': [subnet_id]
+                }
+            ]
+        )
+        nac_id = response['NetworkAcls'][0]["NetworkAclId"]
+        all_instances.append(("Instance id: " + instance_id, "Security Group ID: " + security_group_id, "NACL id: " + nac_id))
+            
 
 def lambda_handler(event, context):
     
-    # Create an array to store the instance id's to loop over later
-    all_instances = []
-    
     # Desribe all instances in this region
-    result = client.describe_instances()
     
-    # Get the instance id from the above call
-    for instance in range(len(result['Reservations'])):
-        # Check if the instance is in the Target VPC:
-        if(result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['VpcId'] == target_vpc_id):
-            instance_id = result['Reservations'][instance]['Instances'][0]['InstanceId']
-            security_group_id = result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['Groups'][0]['GroupId']
-            subnet_id = result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['SubnetId']
-            
-            response = client.describe_network_acls(
-                Filters=[
-                    {
-                        'Name': 'association.subnet-id',
-                        'Values': [subnet_id]
-                    }
-                ]
-            )
-            nac_id = response['NetworkAcls'][0]["NetworkAclId"]
-            all_instances.append(("Instance id: " + instance_id, "Security Group ID: " + security_group_id, "NACL id: " + nac_id))
+    # While True will simulate do-while in python to make the first call, and check if any subsequent calls are needed
+    
+    while(True):
         
-    
-    # Check if 'result' call is paginated, if so repeat the call
-    try:
-        while(result.NextToken):
-            get_token = result.NextToken
-            result = client.describe_instances(NextToken=get_token)
-            
-            for instance in range(len(result['Reservations'])):
-                 # Check if the instance is in the Target VPC:
-                if(result['Reservations'][instance]['Instances'][0]['NetworkInterfaces'][0]['VpcId'] == target_vpc_id):
-                    instance_id = (result['Reservations'][instance]['Instances'][0]['InstanceId'])
-                    all_instances.append(instance_id)
+        # First SDK call to get all instances.
+        result = client.describe_instances()
         
-    # This should populate all the instances in all_instance array. Loop over to get the Security groups.
-    
-    # If no next token found, ignore the above
-    except: 
-        pass
+        # Get the instance id from the above call
+        for instance in range(len(result['Reservations'])):
+            instanceMetadata(instance, result)
+            
+        
+        # Check if 'result' call is paginated, if so repeat the call
+        try:
+            while(result.NextToken):
+                # If the first SDK call was paginated, it will return 'NextToken', use that to make the next SDK calls to desribe other instances.
+                get_token = result.NextToken
+                result = client.describe_instances(NextToken=get_token)
+                
+                # Get the instance id from the above call
+                for instance in range(len(result['Reservations'])):
+                   
+                    instanceMetadata(instance, result)
+            
+        # This should populate all the instances in all_instance array. Loop over to get the Security groups.
+        
+        # If no next token found, break the loop
+        except: 
+            break;
 
 
 
@@ -59,3 +69,6 @@ def lambda_handler(event, context):
 
     for i in all_instances:
         print(i)
+
+    # print(all_instances)
+    
