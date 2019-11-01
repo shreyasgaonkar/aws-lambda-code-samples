adc2d002bc0fd9b7fbe031d860e55babc395ebbf
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2019-11-01 21:35:08 +0000

    Add sender-id.py

---
+++ sns-samples/sender-id.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import boto3
+client = boto3.client('sns')
+
+
+def lambda_handler(event, context):
+
+    # Check senderid requirements here:
+    # https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-countries.html
+
+    message = 'Hello, from AWS SNS!'
+    response = client.publish(
+        PhoneNumber='+353833798840',
+        Message=message,
+        MessageAttributes={
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': 'Godzilla'
+            }})
+
+    print(response)
