443a8dc09dbe593fb2de3d1f319111bdef88d287
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2019-12-19 20:33:59 +0000

    Update lambda-samples/reserved_concurrency.py
    - Add provisioned concurrency metric
    - Fixes issue raised at:
      https://github.com/shreyasgaonkar/aws-lambda-code-samples/issues/3

--- lambda-samples/reserved_concurrency.md
+++ lambda-samples/reserved_concurrency.md
@@ -1,3 +1,18 @@
 This will iterate through all functions in a region (defaults to where Lambda resides) to return the function name where ```Reserve concurrency``` is set - easier to find functions which might be using up the [Unreserved account concurrency](https://aws.amazon.com/about-aws/whats-new/2017/11/set-concurrency-limits-on-individual-aws-lambda-functions/).
 
-**Note**:```client.get_function_concurrency()``` might require a newer boto3 version client. I have used boto3 v1.10.42, and you can check that using ```boto3.__version__```.
+[Newly introduced]() provisioned concurrency can also contribute towards reducing the Unreserved account concurrency count as seen on the dashboard. ```get_provisioned_concurrency_config()``` can be used to extract the reserved concurrency information off a version or alias.
+
+
+**Note**:```client.get_provisioned_concurrency_config & client.get_function_concurrency()``` might require a newer boto3 version client. I have used boto3 v1.10.42, and you can check that using ```boto3.__version__```.
+
+
+**Output**
+
+```
+Reserved concurrency:
+my_function_1: 10
+my_function_2: 5
+=====================
+Provisioned concurrency:
+other_function: 50
+```
--- lambda-samples/reserved_concurrency.py
+++ lambda-samples/reserved_concurrency.py
@@ -8,17 +8,39 @@ import boto3
 client = boto3.client('lambda')
 paginator = client.get_paginator('list_functions')
 
+# Make sure ALL versions are returned for provisioned concurrency
+operation_parameters = {'FunctionVersion': 'ALL'}
+
 # Create an empty set to dump all the individual function's data
 reserved_concurrency = set()
+provisioned_concurrency = set()
+
+
+def get_provisioned_concurrency(function_name, qualifier):
+
+    response = client.get_provisioned_concurrency_config(
+        FunctionName=function_name,
+        Qualifier=qualifier
+    )
+
+    funct = {
+        "Name": function_name,
+        "Provisioned Concurrency": response['AllocatedProvisionedConcurrentExecutions']
+    }
+
+    funct = json.dumps(funct)
+    provisioned_concurrency.add(funct)
 
 
 def lambda_handler(event, context):
     """ Main function to retrieve reserved concurrency """
 
+    # List all functions
     page_iterator = paginator.paginate()
     for page in page_iterator:
         functions = page['Functions']
 
+        # Check for Reserved Concurrency on a function w/o version(s)
         for function in functions:
             response = client.get_function_concurrency(
                 FunctionName=function['FunctionName']
@@ -27,17 +49,57 @@ def lambda_handler(event, context):
             try:
                 funct = {
                     "Name": function['FunctionName'],
-                    "Concurrency": response['ReservedConcurrentExecutions']
+                    "Reserved Concurrency": response['ReservedConcurrentExecutions']
                 }
                 funct = json.dumps(funct)
                 reserved_concurrency.add(funct)
 
             except:
+                # Skip since no Reserved Concurrency
                 pass
 
+    # Check for provisioned concurrency under alias and/or versions
+    page_iterator = paginator.paginate(**operation_parameters)
+    for page in page_iterator:
+        functions = page['Functions']
+
+        for function in functions:
+            if(function['Version'] != '$LATEST'):
+                # Try if get_provisioned_concurrency_config SDK call succeeds
+                try:
+                    # Try for alias
+                    try:
+                        aliases = []
+                        alias = client.list_aliases(
+                            FunctionName=function['FunctionName']
+                        )
+
+                        for i in alias['Aliases']:
+                            aliases.append(i['Name'])
+
+                        for i in aliases:
+                            get_provisioned_concurrency(function['FunctionName'], i)
+                    except:
+                        pass
+
+                    get_provisioned_concurrency(function['FunctionName'], function['Version'])
+
+                except:
+                    # ProvisionedConcurrencyConfigNotFoundException error
+                    # skip since none set
+                    pass
+
+    print("Reserved concurrency:")
     for i in sorted(reserved_concurrency):
         i = json.loads(i)
-        print(f"{i['Name']}: {i['Concurrency']}")
+        print(f"{i['Name']}: {i['Reserved Concurrency']}")
+
+    print("=====================")
+
+    print("Provisioned concurrency:")
+    for i in sorted(provisioned_concurrency):
+        i = json.loads(i)
+        print(f"{i['Name']}: {i['Provisioned Concurrency']}")
 
     return {
         'statusCode': 200,
