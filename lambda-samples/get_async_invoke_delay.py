c58c1b64763a5a269ec930db662cae8107e08d49
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2020-02-03 21:19:26 +0000

    Update get_async_invoke_delay.py
    - Update for plotting custom CloudWatch Metrics to display async delays

--- lambda-samples/get_async_invoke_delay.py
+++ lambda-samples/get_async_invoke_delay.py
@@ -1,4 +1,6 @@
 import datetime
+import boto3
+client = boto3.client('cloudwatch')
 
 
 def lambda_handler(event, context):
@@ -10,9 +12,13 @@ def lambda_handler(event, context):
     # the timestamp when the record was added to the queue which can be used
     # to get the difference in processing times
 
+    delay = getSeconds(event['Records'][0]['eventTime'])
+    print(f"Current Async delay is: {delay} seconds")
+    plotMetric(delay)
+
     return {
         'statusCode': 200,
-        'body': f"Delay is {getSeconds(event['Records'][0]['eventTime'])} seconds"
+        'body': f"Current Async delay is: {delay} seconds"
     }
 
 
@@ -20,3 +26,23 @@ def getSeconds(time):
     t1 = (datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S.%fZ'))
     t2 = (datetime.datetime.utcnow())
     return((t2-t1).total_seconds())
+
+
+def plotMetric(delay):
+    client.put_metric_data(
+        Namespace='AWS/Lambda',
+        MetricData=[
+            {
+                'MetricName': 'Async Delay',
+                'Dimensions': [
+                    {
+                        'Name': 'Async Delay',
+                        'Value': 'Async Delay'
+                    },
+                ],
+                'Timestamp': datetime.datetime.now(),
+                'Value': delay,
+                'Unit': 'Seconds'
+            }
+        ]
+    )
