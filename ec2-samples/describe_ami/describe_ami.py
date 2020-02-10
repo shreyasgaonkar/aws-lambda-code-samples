a52983334f92ba22450b1c6249f24340db34807c
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2020-02-10 22:53:21 +0000

    Update describe_ami.py

--- ec2-samples/describe_ami/describe_ami.py
+++ ec2-samples/describe_ami/describe_ami.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 client = boto3.client('ec2')
 regions = client.describe_regions()['Regions']
@@ -8,6 +7,6 @@ def lambda_handler(event, context):
     for region in regions:
         region_name=region['RegionName']
         ec2 = boto3.client('ec2', region_name=region_name)
-        response = client.describe_images(Owners=['Your-Account-Id'])
+        response = ec2.describe_images(Owners=['Your-Account-Id'])
         print(response)
         print("~~~")
