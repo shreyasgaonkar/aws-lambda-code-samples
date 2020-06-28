8d7f5ec2b87a5b4e3b38d9b315446ee17ed09e3d
shreyasgaonkar <shreyas.gaonkar@yahoo.com>
2020-06-28 15:26:03 +0100

    Improve API performance

--- lambda-samples/reserved_concurrency/reserved_concurrency.py
+++ lambda-samples/reserved_concurrency/reserved_concurrency.py
@@ -1,120 +1,116 @@
 import json
 import os
+import time
+import collections
 import boto3
-
+from prettytable import PrettyTable
 
 # Use paginator from:
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/paginators.html#filtering-results
 # to call recursively on the same API call if next token is returned.
 
-client = boto3.client('lambda')
-paginator = client.get_paginator('list_functions')
+# ==================
+# Override AWS Region below, otherwise will use Lambda function's region
+
+# REGION_NAME = 'us-east-1'
+# ==================
+
+try:
+    REGION_NAME
+except NameError:
+    REGION_NAME = os.environ['AWS_REGION']
+
+LAMBDA_CLIENT = boto3.client('lambda', region_name=REGION_NAME)
+PAGINATOR = LAMBDA_CLIENT.get_paginator('list_functions')
 
 # Make sure ALL versions are returned for provisioned concurrency
-operation_parameters = {'FunctionVersion': 'ALL'}
+OPERATION_PARAMETERS = {'FunctionVersion': 'ALL'}
 
 # Create an empty set to dump all the individual function's data
-reserved_concurrency = set()
-provisioned_concurrency = set()
+RESERVED_CONCURRENCY = set()
+PROVISIONED_CONCURRENCY = {}
+
+# PrettyTable
+PROVISIONED_CONCURRENCY_TABLE = PrettyTable(['FunctionArn', 'Requested', 'Available',
+                                             'Allocated', 'Status', 'Last Modified'])
+RESERVED_CONCURRENCY_TABLE = PrettyTable(['Function Name', 'Reserved Concurrency'])
 
 
-def get_provisioned_concurrency(function_name, qualifier):
+def list_provisioned_concurrency():
+    """Helper function to determine Provisioned Concurrency"""
 
-    response = client.get_provisioned_concurrency_config(
-        FunctionName=function_name,
-        Qualifier=qualifier
-    )
-
-    funct = {
-        "Name": function_name,
-        "Qualifier": qualifier,
-        "Provisioned Concurrency": response['AllocatedProvisionedConcurrentExecutions']
-    }
-
-    funct = json.dumps(funct)
-    provisioned_concurrency.add(funct)
-
-
-def helper_function():
     # List all functions
-    page_iterator = paginator.paginate()
+    page_iterator = PAGINATOR.paginate()
     for page in page_iterator:
         functions = page['Functions']
 
-        # Check for Reserved Concurrency on a function w/o version(s)
         for function in functions:
-            response = client.get_function_concurrency(
+            response = LAMBDA_CLIENT.list_provisioned_concurrency_configs(
                 FunctionName=function['FunctionName']
             )
 
-            try:
-                funct = {
-                    "Name": function['FunctionName'],
-                    "Reserved Concurrency": response['ReservedConcurrentExecutions']
-                }
-                funct = json.dumps(funct)
-                reserved_concurrency.add(funct)
+            if response['ProvisionedConcurrencyConfigs']:
+                for i in response['ProvisionedConcurrencyConfigs']:
+                    PROVISIONED_CONCURRENCY[i['FunctionArn']] = [
+                        i['RequestedProvisionedConcurrentExecutions'], i['AvailableProvisionedConcurrentExecutions'], i['AllocatedProvisionedConcurrentExecutions'], i['Status'], i['LastModified']]
+            else:
+                time.sleep(0.1)
 
-            except Exception as e:
-                print(e)
+            # Check for Reserved Concurrency
+            list_reserved_concurrency(function['FunctionName'])
 
-    # Check for provisioned concurrency under alias and/or versions
-    page_iterator = paginator.paginate(**operation_parameters)
-    for page in page_iterator:
-        functions = page['Functions']
+    # print(PROVISIONED_CONCURRENCY)
 
-        for function in functions:
-            if function['Version'] != '$LATEST':
-                # Try if get_provisioned_concurrency_config SDK call succeeds
-                try:
-                    # Try for alias
-                    try:
-                        aliases = []
-                        alias = client.list_aliases(
-                            FunctionName=function['FunctionName']
-                        )
 
-                        for i in alias['Aliases']:
-                            aliases.append(i['Name'])
+def list_reserved_concurrency(function_name):
+    response = LAMBDA_CLIENT.get_function_concurrency(
+        FunctionName=function_name
+    )
+    try:
+        funct = {
+            "Name": function_name,
+            "Reserved Concurrency": response['ReservedConcurrentExecutions']
+        }
+        funct = json.dumps(funct)
+        RESERVED_CONCURRENCY.add(funct)
 
-                        for i in aliases:
-                            get_provisioned_concurrency(function['FunctionName'], i)
-                    except Exception as e:
-                        print(e)
+    except Exception:
+        pass
 
-                    get_provisioned_concurrency(function['FunctionName'], function['Version'])
-
-                except Exception as e:
-                    # ProvisionedConcurrencyConfigNotFoundException error
-                    print(e)
+    return True
 
 
 def print_function():
+    """Print function data"""
     print("Per function Reserved concurrency:\n")
     count = 0
-    for i in sorted(reserved_concurrency):
+    for i in sorted(RESERVED_CONCURRENCY):
         i = json.loads(i)
-        print(f"{i['Name']} - {i['Reserved Concurrency']}")
+        # print(f"{i['Name']} - {i['Reserved Concurrency']}")
         count += i['Reserved Concurrency']
+        RESERVED_CONCURRENCY_TABLE.add_row([i['Name'], i['Reserved Concurrency']])
     print(f"Total reserved concurrency in {os.environ['AWS_REGION']} region is {count}")
+    print(RESERVED_CONCURRENCY_TABLE)
     print("\n=====================\n")
 
     print("Per qualifier Provisioned concurrency:\n")
     count = 0
-    for i in sorted(provisioned_concurrency):
-        i = json.loads(i)
-        print(f"{i['Name']}:{i['Qualifier']} - {i['Provisioned Concurrency']}")
-        count += i['Provisioned Concurrency']
+    sorted_pc = collections.OrderedDict(sorted(PROVISIONED_CONCURRENCY.items()))
+    for key, value in sorted_pc.items():
+        PROVISIONED_CONCURRENCY_TABLE.add_row(
+            [key, value[0], value[1], value[2], value[3], value[4]])
+        count += value[0]
     print(f"Total provisioned concurrency in {os.environ['AWS_REGION']} region is {count}")
+    print(PROVISIONED_CONCURRENCY_TABLE)
 
 
 def lambda_handler(event, context):
     """ Main function to retrieve reserved concurrency """
 
-    helper_function()
+    list_provisioned_concurrency()
     print_function()
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Completed execution')
+        'body': json.dumps('Completed execution, see function logs.')
     }
