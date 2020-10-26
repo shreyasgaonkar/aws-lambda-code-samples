cf42d62ee0c68fe7e6d5323cbc23910e09d538cb
shreyasgaonkar <shreyas.gaonkar@yahoo.com>
2020-10-26 18:31:02 +0000

    Update SNS-SMS regions

--- sns-samples/sms_month_to_date_spent_usd/sms_month_to_date_spent_usd.py
+++ sns-samples/sms_month_to_date_spent_usd/sms_month_to_date_spent_usd.py
@@ -13,8 +13,7 @@ TIME_DELTA = datetime.timedelta(hours=1)
 TODAY = datetime.date.today()
 YESTERDAY = datetime.datetime(TODAY.year, TODAY.month, TODAY.day) - TIME_DELTA
 TODAY = datetime.datetime(TODAY.year, TODAY.month, TODAY.day)
-REGIONS = ['us-east-1', 'us-west-1', 'us-west-2', 'us-gov-west-1', 'eu-central-1', 'eu-west-1',
-           'ap-northeast-1', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2']
+REGIONS = ['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'me-south-1', 'sa-east-1', 'us-gov-west-1']
 # Regions from: https://docs.aws.amazon.com/sns/latest/dg/sns-supported-regions-countries.html
 
 
