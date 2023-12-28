bb6567681ece064e5ead3f6550dcf5c928c0461c
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2023-12-28 14:51:57 -0800

    Refactor list_account_topic_subscriptions

--- sns-samples/list_account_topic_subscriptions/list_account_topic_subscriptions.py
+++ sns-samples/list_account_topic_subscriptions/list_account_topic_subscriptions.py
@@ -4,13 +4,12 @@ import boto3
 from prettytable import PrettyTable  # Imported as a Lambda layer
 
 # PrettyTable
-TABLE = PrettyTable(['SubscriptionArn', 'Owner', 'Endpoint',
-                     'TopicArn', 'Region'])
+TABLE = PrettyTable(["SubscriptionArn", "Owner", "Endpoint", "TopicArn", "Region"])
 
 # Crete EC2 client to list all regions
-EC2_CLIENT = boto3.client('ec2')
+EC2_CLIENT = boto3.client("ec2")
 EC2_RESPONSE = EC2_CLIENT.describe_regions()
-REGIONS = [region['RegionName'] for region in EC2_RESPONSE['Regions']]
+REGIONS = [region["RegionName"] for region in EC2_RESPONSE["Regions"]]
 
 
 all_topics = []
@@ -18,31 +17,38 @@ result = []
 
 
 def get_region_from_arn(arn):
-    """ Get region from ARN provided """
+    """Get region from ARN provided"""
 
     return arn.split(":")[3]
 
 
-def print_subscriptions(subs):
-    """ Print subs in PrettyTable """
+def print_subscriptions(subscriptions):
+    """Print subs in PrettyTable"""
 
-    for sub in subs:
-        TABLE.add_row([sub['SubscriptionArn'], sub['Owner'], sub['Endpoint'],
-                      sub['TopicArn'], get_region_from_arn(sub['SubscriptionArn'])])
+    for subscription in subscriptions:
+        TABLE.add_row(
+            [
+                subscription["subscriptionscriptionArn"],
+                subscription["Owner"],
+                subscription["Endpoint"],
+                subscription["TopicArn"],
+                get_region_from_arn(subscription["SubscriptionArn"]),
+            ]
+        )
 
 
 def list_subscription(region):
-    """ List all subscriptions through boto3 pagination """
+    """List all subscriptions through boto3 pagination"""
 
-    sns_client = boto3.client('sns', region_name=region)
+    sns_client = boto3.client("sns", region_name=region)
 
     # Paginator to iterate through all results
-    list_subscriptions_paginator = sns_client.get_paginator('list_subscriptions')
+    list_subscriptions_paginator = sns_client.get_paginator("list_subscriptions")
     response_iterator = list_subscriptions_paginator.paginate()
 
     for response in response_iterator:
-        if response['Subscriptions']:
-            print_subscriptions(response['Subscriptions'])
+        if response["Subscriptions"]:
+            print_subscriptions(response["Subscriptions"])
 
 
 # Start multithreading on individual urls
@@ -57,6 +63,6 @@ def lambda_handler(event, context):
     print(TABLE)
 
     return {
-        'statusCode': 200,
-        'body': json.dumps('Completed execution, check CloudWatch logs')
+        "statusCode": 200,
+        "body": json.dumps("Completed execution, check CloudWatch logs"),
     }
